c9471b1fbca50ca1fa76e16fb96b14a177578b3b
David Feinerman <davidafeinerman@gmail.com>
2022-09-27 18:55:55 -0700

    Spruce up main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,19 +8,21 @@ const Container = styled.div`
   text-align: left;
 `;
 
+const NFTWrapper = styled.div``;
+
 export default function Home() {
   const { contract } = useContract(CONTRACT_ADDRESS);
   const { data: nfts, isLoading: isReadingNfts } = useNFTs(contract);
 
   return (
     <div>
-      <div>
-        Own a piece of the internet.{" "}
-        <strong>Your piece of the internet!</strong>{" "}
-        <Link href={"/mint"}>Click here</Link> to mint your twitter profile!
-      </div>
-
       <Container>
+        <p>
+          Own a piece of the internet.{" "}
+          <strong>Your piece of the internet!</strong>{" "}
+          <Link href={"/mint"}>Click here</Link> to mint your Twitter profile!
+        </p>
+
         <h3>Everybody else is doing it:</h3>
         {isReadingNfts ? (
           <p>Loading...</p>
@@ -28,9 +30,20 @@ export default function Home() {
           <div>
             {nfts &&
               nfts.map((nft, index) => (
-                <div key={index}>
-                  <ThirdwebNftMedia metadata={nft.metadata} height={"200"} />
-                </div>
+                <NFTWrapper key={index}>
+                  <div>
+                    <b>{nft.metadata.name}:</b>
+                  </div>
+                  <ThirdwebNftMedia
+                    style={{
+                      display: "inline-block",
+                      border: "1px solid black",
+                      borderRadius: "5px",
+                    }}
+                    metadata={nft.metadata}
+                    height={"200"}
+                  />
+                </NFTWrapper>
               ))}
           </div>
         )}
